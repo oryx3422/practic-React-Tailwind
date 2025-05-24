716aa5483b81df025ee0802d38a933e6ecca5b0c
nik blago <159848859+oryx3422@users.noreply.github.com>
2025-05-24 16:57:09 +0300

    edit imports, change inputRUB

--- converter_rub-euro/src/assets/components/Converter.jsx
+++ converter_rub-euro/src/assets/components/Converter.jsx
@@ -1,6 +1,5 @@
 import { React, useEffect, useState } from "react"
-import { MdCurrencyRuble } from "react-icons/md";
-import { MdEuroSymbol } from "react-icons/md";
+import { MdCurrencyRuble, MdEuroSymbol } from "react-icons/md";
 
 const Converter = () => {
     const [inputRUB, setInputRUB] = useState('')
@@ -35,7 +34,7 @@ const Converter = () => {
         setInputEURO(euro)
         if (euroToRub && euro !== '' && !isNaN(euro)) {
             const rub = parseFloat(euro) * euroToRub
-            setInputRUB(rub)
+            setInputRUB(rub.toFixed(4))
         } else {
             setInputRUB('')
         } 
@@ -48,7 +47,6 @@ const Converter = () => {
     <p className="text-sm font-semibold  ">1 RUB = {euroToRub ? euroToRub.toFixed(2) : '...'} EURO</p>
     <p className="text-sm font-semibold mb-3">1 EURO = {rubToEuro ? rubToEuro.toFixed(4) : '...'} RUB</p>
 
-  {/* Блок 1 */}
   <div className="flex items-center w-full mb-4 p-3 border border-gray-300 rounded-md">
     <input
       type="text"
@@ -60,7 +58,6 @@ const Converter = () => {
     <MdCurrencyRuble className="ml-4 text-2xl text-gray-700" />
   </div>
 
-  {/* Блок 2 */}
   <div className="flex items-center w-full p-3 border border-gray-300 rounded-md">
     <input
       type="text"
